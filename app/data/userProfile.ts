d2fb9788e094901d6177e5528b410c13725b4e55
sstavh <sashatalabirshuk@gmail.com>
2026-03-28 17:44:19 +0200

    login V3v

--- app/data/userProfile.ts
+++ app/data/userProfile.ts
@@ -1,13 +1,25 @@
+import { reactive } from 'vue'
+
 export interface UserProfile {
   firstName: string
   lastName: string
   email: string
   avatar: string
+  country: string
+  purpose: string
+  birthDate: string
+  phone: string
+  registrationCompleted: boolean
 }
 
-export const userProfile: UserProfile = {
-  firstName: 'Іван',
-  lastName: 'Петренко',
-  email: 'ivan.petrenko@gmail.com',
+export const userProfile = reactive<UserProfile>({
+  firstName: '',
+  lastName: '',
+  email: '',
   avatar: '/images/avatar.jpg',
-}
+  country: '',
+  purpose: '',
+  birthDate: '',
+  phone: '',
+  registrationCompleted: false,
+})
