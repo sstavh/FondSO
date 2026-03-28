d3ef1ac321cab596b8d319f06fee2d71a23d0609
sstavh <sashatalabirshuk@gmail.com>
2026-03-28 14:49:59 +0200

    fix bag

---
+++ app/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <h1>FondSO</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 1rem;
+
+}
+</style>
---
+++ app/pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login-page">
+    <h1>Login</h1>
+    <p>Це сторінка входу.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Додайте тут логіку входу, якщо потрібно
+</script>
+
+<style scoped>
+.login-page {
+  padding: 2rem;
+}
+</style>
