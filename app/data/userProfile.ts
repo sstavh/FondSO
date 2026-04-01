17a52fc38d4a3fb454590741205a4693990d0e29
sstavh <sashatalabirshuk@gmail.com>
2026-04-01 10:56:07 +0300

    Balance Card

--- app/data/userProfile.ts
+++ app/data/userProfile.ts
@@ -1,5 +1,7 @@
 import { reactive } from 'vue'
 
+export type Currency = 'UAH' | 'USD' | 'EUR' | 'PLN'
+
 export interface UserProfile {
   firstName: string
   lastName: string
@@ -12,14 +14,47 @@ export interface UserProfile {
   registrationCompleted: boolean
 }
 
-export const userProfile = reactive<UserProfile>({
-  firstName: '',
-  lastName: '',
-  email: '',
-  avatar: '/images/avatar.jpg',
-  country: '',
-  purpose: '',
-  birthDate: '',
-  phone: '',
-  registrationCompleted: false,
+export interface BalanceHistoryItem {
+  date: string
+  value: number
+}
+
+export interface BalanceState {
+  balance: number
+  currency: Currency
+  history: BalanceHistoryItem[]
+}
+
+export const mainStore = reactive<{
+  userProfile: UserProfile
+  balanceStore: BalanceState
+}>({
+  userProfile: {
+    firstName: '',
+    lastName: '',
+    email: '',
+    avatar: '/images/avatar.jpg',
+    country: '',
+    purpose: '',
+    birthDate: '',
+    phone: '',
+    registrationCompleted: false,
+  },
+
+  balanceStore: {
+    balance: 20150,
+    currency: 'UAH',
+    history: [
+      { date: '01.03', value: 18200 },
+      { date: '05.03', value: 17950 },
+      { date: '10.03', value: 18640 },
+      { date: '15.03', value: 19120 },
+      { date: '20.03', value: 18810 },
+      { date: '25.03', value: 19480 },
+      { date: '30.03', value: 20150 },
+    ],
+  },
 })
+
+export const userProfile = mainStore.userProfile
+export const balanceStore = mainStore.balanceStore
