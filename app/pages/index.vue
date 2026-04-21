22990c2081449cd8b17bb89b6964e636af2653f2
sstavh <sashatalabirshuk@gmail.com>
2026-04-21 10:23:34 +0300

    Header Graphs

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-import Header from '~/components/Header.vue';
+import Header from '~/components/Header.vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
+
+const currentView = (route.query.view as string) || 'home'
 </script>
 
 <template>
-    <Header/>
+  <Header :start-view="currentView" />
 </template>
 
 <style scoped>
-.contaoner {padding-top: 30px;
-
+.container {
+  padding-top: 30px;
   margin-left: 330px;
 }
 </style>
