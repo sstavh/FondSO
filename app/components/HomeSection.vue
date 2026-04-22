357acb2d39fa50b44e8573ab33467b819f81573a
sstavh <sashatalabirshuk@gmail.com>
2026-04-22 11:43:46 +0300

    FIXS V1

--- app/components/HomeSection.vue
+++ app/components/HomeSection.vue
@@ -1,33 +1,53 @@
 <script setup lang="ts">
-
-import BalanceCard from '~/components/UserInformationComponents/BalanceCard.vue';
-import TransactionTotalCard from './UserInformationComponents/TransactionTotalCard.vue';
-import TransactionsCircleCard from './UserInformationComponents/TransactionsCircleCard.vue';
+import BalanceCard from './UserInformationComponents/BalanceCard.vue';
 import GoalTargetCard from './UserInformationComponents/GoalTargetCard.vue';
-import TrendingStocksCard from './UserInformationComponents/TrendingStocksCard.vue';
 import LatestDealsCard from './UserInformationComponents/LatestDealsCard.vue';
+import TransactionsCircleCard from './UserInformationComponents/TransactionsCircleCard.vue';
+import TransactionTotalCard from './UserInformationComponents/TransactionTotalCard.vue';
+import TrendingStocksCard from './UserInformationComponents/TrendingStocksCard.vue';
+
+
 </script>
 
 
 <template>
-    <div class="tt">
-         <BalanceCard class="ttB"/>
-        <TransactionTotalCard/>
-</div>
-<TransactionsCircleCard/>
-<GoalTargetCard/>
-<TrendingStocksCard/>
-<LatestDealsCard      />                
-
+ <div class="container-box">
+        <div class="box-one">
+                <BalanceCard class="BalanceCard"/>
+                 <TransactionsCircleCard class="TransactionsCircleCard"/>
+        </div>
+  <div class="box-tu">
+         <GoalTargetCard class="GoalTargetCard "/>
+  <LatestDealsCard class="LatestDealsCard"/>
+  </div>
+ 
+ 
+  <TransactionTotalCard class="TransactionTotalCard"/>
+  <TrendingStocksCard class="TrendingStocksCard"/>
+ </div>
 </template>
 
-<style scoped>
-.ttB{
-        width: 870px;
-        height: 300px;
+<style scoped >
+.box-one{
+        display: flex;
+        gap: 20px;
+        padding-bottom: 20px;
 }
-.tt {
-  display: flex;
-  gap: 15px;
+.TransactionsCircleCard{
+        flex: 1;
+}
+.box-tu{
+        display: flex;
+        gap: 20px;
+        padding-bottom: 20px;
+}
+
+.TransactionTotalCard{
+        margin-bottom: 20px;
+        width: 1060px;
+}
+.TrendingStocksCard{
+        width: 1085px;
+        margin-bottom: 80px;
 }
 </style>
