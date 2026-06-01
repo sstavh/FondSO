02fed25939e43f23ea2b87ac7ff1656755b4a46a
sstavh <sashatalabirshuk@gmail.com>
2026-06-01 19:37:18 +0300

    fix: add runtimeConfig with apiBase env var

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  runtimeConfig: {
-    public: {
-      apiBase: 'http://localhost:8080',
-    },
-  },
-
   css: ['@/assets/css/index.css'],
 
   app: {
