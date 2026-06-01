4d9d3074a1fcd6161b30f4f0399be6632e73f729
sstavh <sashatalabirshuk@gmail.com>
2026-06-01 19:42:50 +0300

    fixxxxs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE ?? 'http://localhost:8080',
+    },
+  },
+
   css: ['@/assets/css/index.css'],
 
   app: {
